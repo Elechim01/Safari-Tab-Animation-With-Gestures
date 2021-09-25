9bb1d6b72cb548fca81121412077f3486e86f345
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-09-25 23:55:27 +0200

    Initial Commit

---
+++ Shared/Safari_Tab_Animation_With_GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Safari_Tab_Animation_With_GesturesApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 25/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Safari_Tab_Animation_With_GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
